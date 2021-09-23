238e5f9b174dc07ea29826c9ca8be48c5267d830
alifhs <alif.hs@outlook.com>
2021-09-23 20:47:21 +0600

    added buttons and alert

--- App.js
+++ App.js
@@ -17,6 +17,10 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import {
@@ -28,66 +32,52 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const [Items, setItems] = useState([
-    {key: 1, item: 'Item 1'},
-    {key: 2, item: 'Item 2'},
-    {key: 3, item: 'Item 3'},
-    {key: 4, item: 'Item 4'},
-    {key: 5, item: 'Item 5'},
-    {key: 6, item: 'Item 6'},
-    {key: 7, item: 'Item 7'},
-    {key: 8, item: 'Item 8'},
-    {key: 44, item: 'Item 9'},
-    {key: 68, item: 'Item 27'},
-    {key: 0, item: 'Item 78'},
-  ]);
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
-  const onRefresh = () => {
-    setRefreshing(true);
-    setImmediate(() => {
-      setItems([
-        ...Items,
-        {key: new Date().valueOf(), item: 'New number added'},
-      ]);
-      setRefreshing(false);
-    });
+  const [name, setName] = useState('');
+  const onInputChange = value => {
+    setName(value);
   };
-  const [Refreshing, setRefreshing] = useState(false);
+
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={Refreshing} onRefresh={onRefresh} />
-      }
-      style={styles.body}>
-      {Items.map(obj => {
-        return (
-          <View style={styles.view1} key={obj.key}>
-            <Text style={styles.text}>{obj.item}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <View style={styles.body}>
+      <Text>Enter you name:</Text>
+      <TextInput style={styles.input} onChangeText={onInputChange}></TextInput>
+
+      {/* <Button title="Submit" /> */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          Alert.alert('Warning', `User typed ${name}`, [{text: 'Ok'}])
+        }}>
+        <Text style={styles.text}>Submit</Text>
+      </TouchableOpacity>
+      <Text style={styles.inputDisplay}>{name}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  body: {},
-  view1: {
-    backgroundColor: '#028900',
-    margin: 10,
+  body: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
   },
-  view2: {
-    backgroundColor: '#8686BF',
-  },
-  view3: {
-    backgroundColor: '#696969',
+  input: {
+    borderColor: 'gray',
+    width: '70%',
+    borderWidth: 2,
   },
   text: {
-    margin: 30,
-    fontSize: 35,
-    color: 'white',
+    color: '#ffffff',
+  },
+  inputDisplay: {
+    marginTop: 10,
+  },
+  button: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
